bf4eb3813569a4eeb9435d0a9b7aedebdc949b71
AlexisCast <castellanos.alexis@hotmail.com>
2023-09-07 00:43:29 -0700

    fetching logic

--- src/store/cart-slice.jsx
+++ src/store/cart-slice.jsx
@@ -1,9 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { uiActions } from './ui-slice';
-
-const baseUrl = import.meta.env.VITE_BASE_FIREBASE;
-
 const initialCartState = {
   items: [],
   totalQuantity: 0
@@ -21,6 +17,7 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -47,49 +44,6 @@ const cartSlice = createSlice({
     }
   }
 });
-
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(`${baseUrl}/cart.json`, {
-        method: 'PUT',
-        body: JSON.stringify(cart)
-      });
-
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-    };
-
-    try {
-      await sendRequest();
-
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!'
-        })
-      );
-    } catch (error) {
-      dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!'
-        })
-      );
-    }
-  };
-};
 
 export const cartActions = cartSlice.actions;
 
