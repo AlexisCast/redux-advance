14219fdc7d2411459bdd12dfd5e05ef819de1c9e
AlexisCast <castellanos.alexis@hotmail.com>
2023-09-06 18:42:40 -0700

    add reduxToolKit & add functionality to CartButton

--- src/components/Cart/CartButton.jsx
+++ src/components/Cart/CartButton.jsx
@@ -1,8 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const ui = useSelector((state) => state.uicartIsVisible);
+
+  const toggleHandler = () => {
+    console.log('1');
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
